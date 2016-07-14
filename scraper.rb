bd08040c1d40b2b6517082e80216be15ce20a170
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-14 22:40:22 +0100

    Update code to current style

--- scraper.rb
+++ scraper.rb
@@ -44,7 +44,7 @@ page.css('.list__row').each do |entry|
     term: 51,
     source: mp_url,
   }
-  data[:photo].prepend(base_url) unless data[:photo].nil? or data[:photo].empty?
+  data[:photo] = URI.join(base_url, data[:photo]).to_s unless data[:photo].to_s.empty?
 
   added += 1
   ScraperWiki.save_sqlite([:name, :term], data)
