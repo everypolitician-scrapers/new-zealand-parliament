f2c297072060844c11074949110d5e60fd9bdc32
Tony Bowden <tony@tmtm.com>
2016-11-28 15:08:07 +0000

    Move body into a scrape_list method
    Gradually migrate towards our new style

--- scraper.rb
+++ scraper.rb
@@ -14,32 +14,36 @@ def noko(url)
   Nokogiri::HTML(open(url).read)
 end
 
-base = 'https://www.parliament.nz/en/mps-and-electorates/members-of-parliament/'
-page = noko(base)
+def scrape_list(url)
+  page = noko(url)
 
-added = 0
-page.css('.list__row').each do |entry|
-  link   = entry.css('.theme__link')
-  mp_url = URI.join(base, link.attr('href').text)
-  mp     = noko(mp_url)
-  body   = mp.css('div.koru-side-holder')
+  added = 0
+  page.css('.list__row').each do |entry|
+    link   = entry.css('.theme__link')
+    mp_url = URI.join(url, link.attr('href').text)
+    mp     = noko(mp_url)
+    body   = mp.css('div.koru-side-holder')
 
-  data = {
-    id: mp_url.to_s.split("/")[-2],
-    name: body.css("div[role='main']").css('h1').inner_text,
-    sort_name: link.inner_text.strip,
-    party: body.css('.informaltable td')[1].inner_text,
-    area:  body.css('.informaltable td')[0].inner_text,
-    photo: body.css('.document-panel__img img/@src').last.text,
-    email: body.css('a.square-btn').attr('href').inner_text.gsub('mailto:',''),
-    facebook: body.css('div.related-links__item a[@href*="facebook"]/@href').text,
-    twitter:  body.css('div.related-links__item a[@href*="twitter"]/@href').text,
-    term: 51,
-    source: mp_url.to_s,
-  }
-  data[:photo] = URI.join(base, data[:photo]).to_s unless data[:photo].to_s.empty?
+    data = {
+      id: mp_url.to_s.split("/")[-2],
+      name: body.css("div[role='main']").css('h1').inner_text,
+      sort_name: link.inner_text.strip,
+      party: body.css('.informaltable td')[1].inner_text,
+      area:  body.css('.informaltable td')[0].inner_text,
+      photo: body.css('.document-panel__img img/@src').last.text,
+      email: body.css('a.square-btn').attr('href').inner_text.gsub('mailto:',''),
+      facebook: body.css('div.related-links__item a[@href*="facebook"]/@href').text,
+      twitter:  body.css('div.related-links__item a[@href*="twitter"]/@href').text,
+      term: 51,
+      source: mp_url.to_s,
+    }
+    data[:photo] = URI.join(url, data[:photo]).to_s unless data[:photo].to_s.empty?
 
-  added += 1
-  ScraperWiki.save_sqlite([:id, :term], data)
+    added += 1
+    warn data
+    ScraperWiki.save_sqlite([:id, :term], data)
+  end
+  puts "  Added #{added} members"
 end
-puts "  Added #{added} members"
+
+scrape_list 'https://www.parliament.nz/en/mps-and-electorates/members-of-parliament/'
